b7073ae99174fb64fe5238370a8315ee2cb80467
osquatorataopilantra <114157700+osquatorataopilantra@users.noreply.github.com>
2022-09-22 09:39:28 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,3 +1,36 @@
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showNumber(3)
+    basic.showNumber(2)
+    basic.showNumber(1)
+    basic.showString("\"Let's Play!\"")
+    basic.pause(randint(1000, 5000))
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        . . # . .
+        . # # # .
+        . # # # .
+        `)
+    basic.pause(1000)
+})
+input.onPinPressed(TouchPin.P2, function () {
+    basic.showLeds(`
+        . . . # #
+        . . . # #
+        . . . # #
+        . . # # #
+        . . # # #
+        `)
+})
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showLeds(`
+        # # # . .
+        # # # . .
+        # # # . .
+        # # . . .
+        # # . . .
+        `)
+})
 basic.forever(function () {
 	
 })
